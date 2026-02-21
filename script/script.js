a881c0c704c504e415157498268efcefe35e732b
tahiamubin <tahia.mubin1@gmail.com>
2026-02-22 00:46:44 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,21 @@
+
+
+
+
+let total = document.getElementById("mobile-crop-card");
+let webFlowContainer = document.getElementById("webFlow-card");
+let datavizContainer = document.getElementById("dataviz-card");
+let cloudContainer = document.getElementById("cloud-first-card");
+let innovationContainer = document.getElementById("Innovation-labs-card");
+let megaCorpContainer = document.getElementById("megaCorp-solutions-card");
+let startupXYZContainer = document.getElementById("StartupXYZ-card");
+let TechCorpContainer = document.getElementById("TechCorp-industries-card");
+const allCardSection = getElementById("all-card");
+// console.log(allCardSection);
+
+
+function count () {
+    total.innerText = allCardSection.children.length   
+}
+count()
+
