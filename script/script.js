ef95a04acb26d031d701ce47f131e231ff26d360
tahiamubin <tahia.mubin1@gmail.com>
2026-02-22 22:39:25 +0600

    made interview button dynamic

--- script/script.js
+++ script/script.js
@@ -28,7 +28,7 @@ function count () {
     rejected.innerText = rejectedList.length
 
 }
-count();
+
 
 function toggleStyle (id){
     allBtn.classList.remove('bg-[#3B82F6]', 'text-white');
@@ -42,6 +42,15 @@ function toggleStyle (id){
     const selected = document.getElementById(id);
     selected.classList.remove('bg-gray-200', 'text-black')
     selected.classList.add('bg-[#3B82F6]', 'text-white')
+
+    if(id === 'interview-btn'){
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+    }
+    else if (id==="all-btn"){
+        allCardSection.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+    }
 }
 
 mainContainer.addEventListener('click', function(event){
@@ -55,49 +64,46 @@ mainContainer.addEventListener('click', function(event){
  const status = parentNode.querySelector('.status').innerText;
  const para3 = parentNode.querySelector('.para3').innerText;
 
+  parentNode.querySelector('.status').innerText = 'Interview';
+
  const jobInfo ={
     jobName, 
     para1, 
     para2 , 
-    status , 
+    status: 'Interview' , 
     para3
  }
- const jobExits = interviewList.find(item=> item.jobName == jobInfo.jobName )
+ const jobExits = interviewList.find(item=> item.jobName == jobInfo.jobName );
+
    if (!jobExits ){
     interviewList.push(jobInfo);
    }
+   count ();
    renderInterview();
    //console.log(interviewList);
  }
 
 });
 
-function renderInterview(){
- filterSection.innerHTML = '';
- for(interview of interviewList){
-    console.log(interview);
-    let div = document.createElement ('div');
-    div.className =     `flex justify-between shadow-lg bg-blue-100 rounded-lg p-4 my-4`;
-    div.innerHTML =     `
-      <div id="mobile-crop">
-            <div id="crop-heading">
-            <h1 class="text-blue-900 text-xl font-bold py-4  job-name">Mobile First Corp</h1>
-            <p class="text-gray-400 para1">React Native Developer</p>
-            <p class="text-gray-400 para2">Remote • Full-time •$130,000 - $175,000</p>
-            </div>
-            <div id="not-applied" >
-                <button class="bg-gray-100 text-blue-950 my-4  p-4 rounded-md  status">Not Applied</button>
-                <p class="my-4 para3">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
-            </div>
-            <div class="job-btn">
-                <button id="interview-btn" class="border-2 bg-green-400 py-2 px-4 text-white" >Interview</button>
-                <button id="rejected-btn" class="border-2 bg-red-400 py-2 px-4 text-white" >Rejected</button>
-            </div>
-        </div>
-         <div id="delete">
-        <button class="bg-red-200 text-red-800 p-2 rounded-md">Delete</button>
-       </div>
-    `
- }
+function renderInterview() {
+  filterSection.innerHTML = '';
+
+  for (let item of interviewList) {
+    let div = document.createElement('div');
+    div.className =
+      'flex justify-between shadow-lg bg-blue-100 rounded-lg p-4 my-4';
+
+    div.innerHTML = `
+      <div>
+        <h1 class="text-blue-900 text-xl font-bold py-4 job-name">${item.jobName}</h1>
+        <p class="text-gray-400 para1">${item.para1}</p>
+        <p class="text-gray-400 para2">${item.para2}</p>
+        <button class="bg-gray-100 text-blue-950 my-4 p-4 rounded-md status">${item.status}</button>
+        <p class="my-4 para3">${item.para3}</p>
+      </div>
+    `;
+
+    filterSection.appendChild(div);
+  }
 }
 
